d0db1ed6839f9947c4cd18cc645b050035c90ddd
naoto hamada <hamada@vis-its.com>
2019-09-26 15:09:06 +0900

    add rspec, add mutation for create link

---
+++ app/graphql/queries/all_links.rb
@@ -0,0 +1,14 @@
+module Queries
+  class AllLinks < BaseQuery
+    type [Types::LinkType], null: false
+    description "Query all Links"
+
+    argument :description, String, required: false, default_value: nil
+
+    def resolve(description:)
+      return Link.all unless description
+
+      Link.where('description like ?', "%#{description}%")
+    end
+  end
+end
