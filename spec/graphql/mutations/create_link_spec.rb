f709751245ade82051280e8b1fd9f2848e131add
hamadanaoto <teiru_3@yahoo.co.jp>
2020-02-13 23:22:18 +0900

    Fix error spec

--- spec/graphql/mutations/create_link_spec.rb
+++ spec/graphql/mutations/create_link_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Mutations::CreateLink do
-  subject(:mutation) { described_class.new(object: nil, context: {}).resolve(args) }
+  subject(:mutation) { described_class.new(object: nil, context: {}, field: nil).resolve(args) }
 
   let(:args) { { url: url, description: description } }
   let(:url) { 'http://example.com' }
   let(:description) { 'description' }
 
   context 'データが正しく入力されている場合' do
-    xit 'linkが新規登録されること' do
+    it 'linkが新規登録されること' do
       expect(mutation.description).to eq description
       expect(mutation.url).to eq url
     end
