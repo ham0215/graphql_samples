fbc6dff3080628f9f8f1b77339a2c65b92f88210
hamadanaoto <teiru_3@yahoo.co.jp>
2020-02-16 12:26:50 +0900

    Add examinee including devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_041602) do
+ActiveRecord::Schema.define(version: 2020_02_16_032002) do
+
+  create_table "examinees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_examinees_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_examinees_on_reset_password_token", unique: true
+  end
 
   create_table "links", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "url"
---
+++ spec/factories/examinees.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :examinee do
+    
+  end
+end
