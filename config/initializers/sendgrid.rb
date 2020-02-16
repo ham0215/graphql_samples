47f9d540062e86a7531f5d1638fb700d1eff39af
hamadanaoto <teiru_3@yahoo.co.jp>
2020-02-16 13:26:06 +0900

    Add sendgrid settings

---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  user_name: Rails.application.credentials.send_grid.user_name,
+  password: Rails.application.credentials.send_grid.password,
+  domain: Rails.application.credentials.send_grid.domain,
+  address: Rails.application.credentials.send_grid.address,
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
