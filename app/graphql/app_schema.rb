3b6145f5e3ed3f6fd7c88574aa9f9c1722ef4ad4
naoto hamada <hamada@vis-its.com>
2020-07-23 23:11:24 +0900

    Update schema

--- app/graphql/app_schema.rb
+++ app/graphql/app_schema.rb
@@ -1,4 +1,9 @@
 class AppSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
+
+  use GraphQL::Execution::Interpreter
+  use GraphQL::Analysis::AST
+
+  use GraphQL::Pagination::Connections
 end
