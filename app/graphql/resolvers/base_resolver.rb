3f6cd597a8f4480e3dbcad1dd39ef8b062d4cb5a
naoto hamada <hamada@vis-its.com>
2020-07-24 14:09:00 +0900

    Add login_required!

--- app/graphql/resolvers/base_resolver.rb
+++ app/graphql/resolvers/base_resolver.rb
@@ -1,4 +1,8 @@
 module Resolvers
   class BaseResolver < GraphQL::Schema::Resolver
+    def login_required!
+      # ログインしていなかったらraise
+      raise unless context[:current_user]
+    end
   end
 end
