d9a673841e097183612be582bb785e715c5dc9da
naoto hamada <hamada@vis-its.com>
2020-07-26 14:10:10 +0900

    Add user_id, secret to review_type

--- app/graphql/types/review_type.rb
+++ app/graphql/types/review_type.rb
@@ -2,6 +2,8 @@ module Types
   class ReviewType < BaseObject
     field :id, ID, null: false
     field :title, String, null: true
-    field :body, AuthScalar, null: true
+    field :body, String, null: true
+    field :secret, String, null: true
+    field :user, Types::UserType, null: false
   end
 end
