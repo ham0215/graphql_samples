b29a171b16cbbbaac312576b9108774b9170cd68
naoto hamada <hamada@vis-its.com>
2020-07-27 22:55:49 +0900

    Fix rubocop

--- app/graphql/types/my_review_type.rb
+++ app/graphql/types/my_review_type.rb
@@ -2,6 +2,7 @@ module Types
   class MyReviewType < ReviewType
     def self.authorized?(object, context)
       raise GraphQL::ExecutionError, 'permission denied!!' if context[:current_user].id != object.user_id
+
       true
     end
   end
