d900848e006d19610cfe1f15e6f43f9e52009881
naoto hamada <hamada@vis-its.com>
2020-12-15 15:40:04 +0900

    Fix rubocop

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,3 @@
 class Character < ApplicationRecord
-  delegated_type :char, types: %w[ Human Droid Starship ], dependent: :destroy
+  delegated_type :char, types: %w[Human Droid Starship], dependent: :destroy
 end
--- app/models/human.rb
+++ app/models/human.rb
@@ -1,5 +1,5 @@
 class Human < ApplicationRecord
   # humenを見に行ってしまうので
-  self.table_name = "humans"
+  self.table_name = 'humans'
   include Char
 end
