fed07f164a6508a726b4a7feb19b562bdfd18eca
naoto hamada <hamada@vis-its.com>
2020-12-16 08:15:35 +0900

    Rename

--- db/migrate/20201215031635_create_table_delegated_type.rb
+++ db/migrate/20201215031635_create_table_delegated_type.rb
@@ -1,4 +1,4 @@
-class CreateTableDelegatingType < ActiveRecord::Migration[6.1]
+class CreateTableDelegatedType < ActiveRecord::Migration[6.1]
   def change
     create_table :characters do |t|
       t.string :name, null: false
