1d8a71aef0319144cd4423005999a9a1ebc25882
naoto hamada <hamada@vis-its.com>
2021-02-13 22:44:47 +0900

    Update base class

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -2,4 +2,12 @@ module Mutations
   class BaseMutation < GraphQL::Schema::Mutation
     null false
   end
+=begin
+  class BaseMutation < GraphQL::Schema::RelayClassicMutation
+    argument_class Types::BaseArgument
+    field_class Types::BaseField
+    input_object_class InputObjects::BaseInputObject
+    object_class Types::BaseObject
+  end
+=end
 end
