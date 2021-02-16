1bc101770ef46f1a366dfd34b386141e0af468e2
naoto hamada <hamada@vis-its.com>
2021-02-16 09:38:50 +0900

    Fix user schema (#30)

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,6 +5,6 @@ module Types
     # we are exposing `email` just for tutorial purposes
     # in real application shouldn't leak user emails
     field :email, String, null: false
-    field :user_organizations, UserOrganizationType.connection_type, null: false
+    field :organizations, OrganizationType.connection_type, null: false
   end
 end
